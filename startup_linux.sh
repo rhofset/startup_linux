e70079b29d84b1b61c8d3ac235b33f522db5c7c6
rhofset <rhofset@gmail.com>
2019-04-08 21:44:06 +0200

    Update startup_linux.sh
    Updated vs code

--- startup_linux.sh
+++ startup_linux.sh
@@ -20,4 +20,7 @@ sudo apt-get update
 sudo apt-get install Filezilla
 
 # Visual Code
-sudo apt install ./https://go.microsoft.com/fwlink/?LinkID=760868
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
