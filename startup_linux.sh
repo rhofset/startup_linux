472a70335e60613efae6bfb954c006c14f7a64de
rhofset <rhofset@gmail.com>
2019-04-08 21:22:27 +0200

    Update startup_linux.sh
    added update and upgrade

--- startup_linux.sh
+++ startup_linux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Check if file is run as root
 if [ "$EUID" -ne 0 ]
     then echo "Please run this file as root"
     exit
 fi
 
+# Update
+sudo apt-get update
+
+# Upgrade
+sudo apt-get upgrade
