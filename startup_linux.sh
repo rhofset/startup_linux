d44972998d2b31c6ed280d9b9e0f789d6928e05d
rhofset <rhofset@gmail.com>
2019-04-10 22:10:18 +0200

    Update startup_linux.sh
    added nfs-common packet

--- startup_linux.sh
+++ startup_linux.sh
@@ -24,3 +24,6 @@ curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
+
+# nfs-common
+sudo apt-get install nfs-common
